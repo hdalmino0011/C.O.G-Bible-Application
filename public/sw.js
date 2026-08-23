aad38d9cef322e890e928e3d11c5cc8ed8c57633
hdalmino0011 <dalminohanz14@gmail.com>
2026-08-23 20:39:54 +0800

    Update cache version to clear old compiled assets with Pinadayag reference

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cog-bible-v1.3.0';
+const CACHE_NAME = 'cog-bible-v1.3.1';
 const STATIC_ASSETS = __PRECACHE_ASSETS__;
 
 // Install: Cache essential assets including the entire offline verses.json database
